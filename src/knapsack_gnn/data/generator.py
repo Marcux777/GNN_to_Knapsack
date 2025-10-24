20c01e73b5eb87e2a8e6750e58d38cd501733371
Marcux777 <marcux7777@gmail.com>
2025-10-23 22:57:02 -0300

    Auto-commit: Edit modified generator.py
    - File: /home/marcusvinicius/Void/GNN_to_Knapsack/src/knapsack_gnn/data/generator.py
    - Tool: Edit
    - Session: 6267da0a-af7e-4bb7-88de-6f8790f66214

--- src/knapsack_gnn/data/generator.py
+++ src/knapsack_gnn/data/generator.py
@@ -347,8 +347,8 @@ def solve_knapsack_dp(
     Returns:
         Tuple of (optimal_value, solution)
     """
-    # KnapsackInstance constructor takes (weights, values, capacity)
-    instance = KnapsackInstance(weights, values, capacity)
+    # Create instance with correct parameter order (weights, values, capacity)
+    instance = KnapsackInstance(weights=weights, values=values, capacity=capacity)
     solved = KnapsackSolver.solve(instance, time_limit=time_limit)
     return solved.optimal_value, solved.solution
 
